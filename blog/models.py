44c847ec854a2b18d7708db607452d69d7d7ec4e
Sara Monalisa <saramonaliscs@gmail.com>
2024-09-06 10:21:04 -0300

    Adiconando conteúdo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    categoria = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=600)
+    conteudo = models.TextField(max_length=1200)
+    imagem = models.ImageField(blank=True)
+    
+    def __str__(self):
+        return self.titulo
