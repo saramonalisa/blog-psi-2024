a19b282143908fffb565ec6b9cbc1519c38b3fc5
Sara Monalisa <saramonaliscs@gmail.com>
2024-09-20 10:17:54 -0300

    Mudando para o site do 2o bimestre

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Post(models.Model):
-    categoria = models.CharField(max_length=100)
     titulo = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=600)
-    conteudo = models.TextField(max_length=1200)
+    conteudo = models.TextField(max_length=5000)
     imagem = models.ImageField(blank=True, upload_to='media/')
     
     def __str__(self):
         return self.titulo
+    
+# tem que ter 2 models e relação entre eles
