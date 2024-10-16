3cd23d1d22b90823628c3bf245de708aefc4b622
Sara <saramonaliscs@gmail.com>
2024-10-16 16:30:51 -0300

    Consertando configuracoes

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100)
     conteudo = models.TextField(max_length=5000)
-    imagem = models.ImageField(blank=True, upload_to='media/')
+    imagem = models.ImageField(blank=True)
     
     def __str__(self):
         return self.titulo
